a128a1318b67faf81687527955885ae42ed641c8
SHERA RAM <56213479+sheraram@users.noreply.github.com>
2024-01-05 15:52:28 +0530

    Create Post page and basic API call created

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+'use client';
 
-const page = () => {
+import Form from '@components/Form';
+import { useState } from 'react';
+
+const CreatePrompt = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: ''
+  });
+
+  const createPrompt = async (e) => {
+    e.preventDefault(); // prevent default behavior of browser when submitting a form which is to do reload in react and xjs application -  We want the least amount of reload as possible
+
+    setSubmitting(true);
+
+    try {
+      const response = await fetch('api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag
+        })
+      });
+
+      if (response.ok) {
+        router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
   return (
-    <div>page</div>
-  )
-}
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={setSubmitting}
+    />
+  );
+};
 
-export default page
+export default CreatePrompt;
