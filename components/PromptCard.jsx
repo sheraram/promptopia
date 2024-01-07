974c95ea55326f00081a0e0f86842403ce6865c9
SHERA RAM <56213479+sheraram@users.noreply.github.com>
2024-01-08 01:25:13 +0530

    alt issue solved

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -58,6 +58,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             }
             width={12}
             height={12}
+            alt={copied === post.prompt ? 'copied' : 'copy'}
           />
         </div>
       </div>
